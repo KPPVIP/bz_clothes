6c53984e1ccfb3fb5a6ad59a59bb05a4b3c89cbf
Guudz <Kilian1702@outlook.com>
2023-02-13 16:27:49 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'BzZz'
+description 'DLC Clothes - FREE RESOURCE'
+version '1.0.0'
+
+files {
+  'mp_m_freemode_01_mp_m_bz_dlc_clothes.meta',
+  'mp_f_freemode_01_mp_f_bz_dlc_clothes.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_bz_dlc_clothes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_bz_dlc_clothes.meta'
